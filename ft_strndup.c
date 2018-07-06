748b99ceacb6ea0ce8d6c8d7ce0f52290a1fd77a
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-07-06 13:12:11 +0200

    fix strndup.c

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/01 16:08:01 by ebouvier          #+#    #+#             */
-/*   Updated: 2018/07/01 16:09:39 by ebouvier         ###   ########.fr       */
+/*   Updated: 2018/07/06 13:11:57 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 char	*ft_strndup(const char *s1, size_t n)
 {
-	size_t	len;
 	char	*s;
 
-	len = ft_strlen(s1);
 	s = (char*)malloc(sizeof(*s) * (n + 1));
 	if (!s)
 		return (NULL);
